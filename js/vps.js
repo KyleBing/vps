c6689f286b3ec47371446e3d0f09bb1afd84bc16
KyleBing <kylebing@163.com>
2022-11-02 21:34:58 +0800

    ^

--- js/vps.js
+++ js/vps.js
@@ -22,37 +22,18 @@ const app = Vue.createApp({
             this.isLoading = true
             const url = '../../portal/vps/justmysocks'
             // const url = 'https://kylebing.cn/portal/vps/justmysocks'
-
-            this.info = {
-                "monthAmount": 500,
-                "monthUsage": 0.6,
-                "monthResetDay": 2
-            }
-
-            this.percentageData = Number(((1 - (this.info.monthUsage / this.info.monthAmount)) * 100).toFixed(2))
-
-
-/*            axios
+            axios
                 .get(url)
                 .then(res => {
                     // stop loading animation
                     this.isLoading = false
-
                     this.info = res.data.data
-
-                    this.info = {
-                        "monthAmount": 500,
-                        "monthUsage": 0.6,
-                        "monthResetDay": 2
-                    }
-
-                    this.percentageData = Number(((1 - (info.monthUsage / info.monthAmount)) * 100).toFixed(2))
-
+                    this.percentageData = Number(((1 - (this.info.monthUsage / this.info.monthAmount)) * 100).toFixed(2))
                 })
                 .catch(err => {
                     // stop loading animation
                     this.isLoading = false
-                })*/
+                })
         },
 
 
